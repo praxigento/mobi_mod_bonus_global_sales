bd1d8978b3c2bd9886f32d55cdf7516d7ab7d6b1
Alex Gusev <alex@flancer64.com>
2016-03-31 15:08:32 +0300

    MOBI-265 - data setup migration

--- src/Setup/InstallData.php
+++ src/Setup/InstallData.php
@@ -8,12 +8,39 @@ namespace Praxigento\BonusGlobalSales\Setup;
 
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Praxigento\Accounting\Lib\Entity\Type\Operation as TypeOperation;
+use Praxigento\Bonus\Base\Lib\Entity\Type\Calc as TypeCalc;
+use Praxigento\Bonus\GlobalSales\Lib\Config as Cfg;
 
-class InstallData extends \Praxigento\Core\Setup\Data\Base {
-    protected function _setup(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+class InstallData extends \Praxigento\Core\Setup\Data\Base
+{
+    private function _addAccountingOperationsTypes()
     {
-        // TODO: Implement _setup() method.
+        $this->_getConn()->insertArray(
+            $this->_getTableName(TypeOperation::ENTITY_NAME),
+            [TypeOperation::ATTR_CODE, TypeOperation::ATTR_NOTE],
+            [
+                [Cfg::CODE_TYPE_OPER_BONUS, 'Global Sales bonus.']
+            ]
+        );
     }
 
+    private function _addBonusCalculationsTypes()
+    {
+        $this->_getConn()->insertArray(
+            $this->_getTableName(TypeCalc::ENTITY_NAME),
+            [TypeCalc::ATTR_CODE, TypeCalc::ATTR_NOTE],
+            [
+                [Cfg::CODE_TYPE_CALC_QUALIFICATION, 'Qualification for Global Sales bonus.'],
+                [Cfg::CODE_TYPE_CALC_BONUS, 'Global Sales bonus itself.']
+            ]
+        );
+    }
+
+    protected function _setup(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $this->_addAccountingOperationsTypes();
+        $this->_addBonusCalculationsTypes();
+    }
 
 }
